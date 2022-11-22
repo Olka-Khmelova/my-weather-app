f959e3e169b0ab5d4239c419cd9f1ea08e2cf14e
Olka-Khmelova <khmeleva_olga@ukr.net>
2022-11-22 21:21:02 +0200

    Improved some elementary stylese. Removed background image.

--- script/script.js
+++ script/script.js
@@ -26,15 +26,18 @@ let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${hour}:${minutes}`;
 
 //////////////////////     Search form      /////////////////////
-function currentWeather(response) {
+function showWeather(response) {
+  celsiusTemperature = response.data.main.temp;
+  feelsTemperature = response.data.main.feels_like;
+
   let displayCity = document.querySelector("#display-city");
   displayCity.innerHTML = response.data.name;
 
   let displayTemperature = document.querySelector("#current-temperature");
-  displayTemperature.innerHTML = Math.round(response.data.main.temp);
+  displayTemperature.innerHTML = Math.round(celsiusTemperature);
 
-  let feelsTemperature = document.querySelector("#feels-temperature");
-  feelsTemperature.innerHTML = Math.round(response.data.main.feels_like);
+  let displayFeelsTemperature = document.querySelector("#feels-temperature");
+  displayFeelsTemperature.innerHTML = Math.round(feelsTemperature);
 
   let weatherDescr = document.querySelector("#weather-description");
   weatherDescr.innerHTML = response.data.weather[0].main;
@@ -44,14 +47,21 @@ function currentWeather(response) {
 
   let windSpeed = document.querySelector("#wind-speed-info");
   windSpeed.innerHTML = Math.round(response.data.wind.speed);
+
+  let iconElement= document.querySelector("#icon");
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
+
 function getPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}`;
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(currentWeather);
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
 }
 
 function currentCity(event) {
@@ -61,30 +71,10 @@ function currentCity(event) {
 let currentCityBtn = document.querySelector(".geo-location-button");
 currentCityBtn.addEventListener("click", currentCity);
 
-function showWeather(response) {
-  let displayCity = document.querySelector("#display-city");
-  displayCity.innerHTML = response.data.name;
-
-  let displayTemperature = document.querySelector("#current-temperature");
-  displayTemperature.innerHTML = Math.round(response.data.main.temp);
-
-  let feelsTemperature = document.querySelector("#feels-temperature");
-  feelsTemperature.innerHTML = Math.round(response.data.main.feels_like);
-
-  let weatherDescr = document.querySelector("#weather-description");
-  weatherDescr.innerHTML = response.data.weather[0].main;
-
-  let humidity = document.querySelector("#humidity-info");
-  humidity.innerHTML = response.data.main.humidity;
-
-  let windSpeed = document.querySelector("#wind-speed-info");
-  windSpeed.innerHTML = Math.round(response.data.wind.speed);
-}
 function search(city) {
   let apiKey = "fe1483f743b581b5520a1b725af03a49";
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}`;
-  console.log(apiUrl);
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showWeather);
 }
 
@@ -93,6 +83,45 @@ function searchingSubmit(event) {
   let city = document.querySelector("#city-input").value;
   search(city);
 }
+function showFahrenh (event){
+  event.preventDefault();
+  let displayTemperature = document.querySelector("#current-temperature");
+  let displayFeelsTemperature = document.querySelector("#feels-temperature");
+  fahrenheitLink.classList.add("active");
+  celsiusLink.classList.remove("active");
+
+
+  let fahrenhTemperature = (celsiusTemperature*9)/5 + 32;
+  let fahrenhFeels = (feelsTemperature*9)/5 + 32;
+
+  displayTemperature.innerHTML = Math.round(fahrenhTemperature);
+  displayFeelsTemperature.innerHTML = `${Math.round(fahrenhFeels)} F`;
+}
+
+function showCelsius (event){
+  event.preventDefault();
+  let displayTemperature = document.querySelector("#current-temperature");
+  displayTemperature.innerHTML = Math.round(celsiusTemperature);
+
+  
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+ 
+  let displayFeelsTemperature = document.querySelector("#feels-temperature");
+  displayFeelsTemperature.innerHTML = `${Math.round(feelsTemperature)} °C`;
+}
+
+let celsiusTemperature = null;
+let feelsTemperature = null;
+
 let serchingForm = document.querySelector("#searching-form");
 serchingForm.addEventListener("submit", searchingSubmit);
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFahrenh);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCelsius);
+
 search("Kyiv");
